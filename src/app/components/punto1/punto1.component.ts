caeb3b0cfdc89c45436ca677cb848401fc540465
conradovargas93 <conradovargas93@gmail.com>
2025-04-22 20:18:23 +0000

    feat(punto1): crear componente punto1

---
+++ src/app/components/punto1/punto1.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-punto1',
+  imports: [],
+  templateUrl: './punto1.component.html',
+  styleUrl: './punto1.component.css'
+})
+export class Punto1Component {
+
+}
