84344319a41e4b8f17b5ef8e7d06c41184fabe23
conradovargas93 <conradovargas93@gmail.com>
2025-04-22 04:53:42 +0000

    fix: corrige exportación de componentes

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -4,7 +4,8 @@ import { Component } from '@angular/core';
   selector: 'app-home',
   imports: [],
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrl: './home.component.css'
+})
 export class HomeComponent {
 
 }
