bd7b57b01439f353afd64fbb2b94d23f001b5886
conradovargas93 <conradovargas93@gmail.com>
2025-04-26 00:58:12 +0000

    feat(punto2): crear componente base para productos destacados

---
+++ src/app/punto2/punto2.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-punto2',
+  imports: [],
+  templateUrl: './punto2.component.html',
+  styleUrl: './punto2.component.css'
+})
+export class Punto2Component {
+
+}
